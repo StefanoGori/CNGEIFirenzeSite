acd77849f6edbbe3d7e1128abd593e140a6eb0ae
StefanoGori <stefano.gori.gs@gmail.com>
2025-04-18 01:39:00 +0200

    da risolvere apertura barra ricerca

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule],
+  imports: [MatButtonModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
-
+  showSearchBar=false;
+  searchTerm='';
 
   openChiSiamoUl(){
     const chiSiamoUl=document.getElementById('chiSiamoUl');
@@ -20,5 +24,14 @@ export class ToolbarComponent {
   openContributiUl(){
     const contributiUl=document.getElementById('contributiUl');
     contributiUl?.classList.toggle('visible');
+  }
+
+  openSearchBar(){
+    this.showSearchBar=!this.showSearchBar;
+  }
+
+
+  executeSearch(){
+
   }
 }
