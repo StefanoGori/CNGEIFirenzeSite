828ad323beab5d938c13322ca75f2be0a5a543a0
StefanoGori <stefano.gori.gs@gmail.com>
2025-06-20 10:00:02 +0200

    Fatta home + aggiunto settaggio docker

--- frontend/src/app/pages/chiSiamo/gruppi/gruppi.component.ts
+++ frontend/src/app/pages/chiSiamo/gruppi/gruppi.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-gruppi',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './gruppi.component.html',
   styleUrl: './gruppi.component.css'
 })
 export class GruppiComponent {
+
+  gruppi = [
+    { nome: 'Gruppo Firenze 1', descrizione: 'svolge le sue attività nelle zone di Novoli, Rifredi e Castello.' },
+    { nome: 'Gruppo Firenze 2', descrizione: 'svolge le sue attività nelle zone delle Cure, Coverciano, Campo di Marte e del Centro Storico.' },
+    { nome: 'Gruppo Firenze 3', descrizione: 'svolge le sue attività nelle zone di Isolotto, Scandicci, Porta Romana, Oltrarno.' },
+    { nome: 'Gruppo Firenze 4', descrizione: 'svolge le sue attività nelle zone di Gavinana, Galluzzo, Bagno a Ripoli, Grassina e Antella.' },
+    { nome: 'Gruppo Firenze 5', descrizione: 'svolge le sue attività a Campi Bisenzio, San Donnino e nelle zone limitrofe.' },
+    { nome: 'Gruppo Firenze 6', descrizione: 'svolge le sue attività a Sesto Fiorentino, Calenzano e nelle zone limitrofe.' }
+  ];
+
 
 }
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,33 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule, CommonModule],
+  imports: [MatGridListModule, CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
   articles = [
-    { id: 1, title: "Ultimo articolo" },
-    { id: 2, title: "Secondo articolo" },
-    { id: 3, title: "Terzo articolo" },
-    { id: 4, title: "Quarto articolo" },
-    { id: 5, title: "Quinto articolo" },
-    { id: 6, title: "Sesto articolo" },
-    { id: 7, title: "Settimo articolo" },
-    { id: 8, title: "Ottavo articolo" },
-    { id: 9, title: "Nono articolo" },
-    { id: 10, title: "Decimo articolo" },
-    { id: 11, title: "Undicesimo articolo" },
-    { id: 12, title: "Dodicesimo articolo" },
-    { id: 13, title: "Tredicesimo articolo" },
-    { id: 14, title: "Quattordicesimo articolo" },
-    { id: 15, title: "Quindicesimo articolo" }
+    { id: 1, title: "Ultimo articolo", image: "https://picsum.photos/200/300?random=1" },
+    { id: 2, title: "Secondo articolo", image: "https://picsum.photos/200/300?random=2" },
+    { id: 3, title: "Terzo articolo", image: "https://picsum.photos/200/300?random=3" },
+    { id: 4, title: "Quarto articolo", image: "https://picsum.photos/200/300?random=4" },
+    { id: 5, title: "Quinto articolo", image: "https://picsum.photos/200/300?random=5" },
+    { id: 6, title: "Sesto articolo", image: "https://picsum.photos/200/300?random=6" },
+    { id: 7, title: "Settimo articolo", image: "https://picsum.photos/200/300?random=7" },
+    { id: 8, title: "Ottavo articolo", image: "https://picsum.photos/200/300?random=8" },
+    { id: 9, title: "Nono articolo", image: "https://picsum.photos/200/300?random=9" },
+    { id: 10, title: "Decimo articolo", image: "https://picsum.photos/200/300?random=10" },
+    { id: 11, title: "Undicesimo articolo", image: "https://picsum.photos/200/300?random=11" },
+    { id: 12, title: "Dodicesimo articolo", image: "https://picsum.photos/200/300?random=12" },
+    { id: 13, title: "Tredicesimo articolo", image: "https://picsum.photos/200/300?random=13" },
+    { id: 14, title: "Quattordicesimo articolo", image: "https://picsum.photos/200/300?random=14" },
+    { id: 15, title: "Quindicesimo articolo", image: "https://picsum.photos/200/300?random=15" }
   ];
 
 }
